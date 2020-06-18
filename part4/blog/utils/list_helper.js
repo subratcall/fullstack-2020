61b4e7c5075545039be146b15ea43112b2a9c7a9
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-06-17 20:15:49 -0400

    4.6 & 4.7

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,3 +1,9 @@
+const arrayMax = array => {
+	return array.length === 0
+		? 0
+		: Math.max(...array)
+}
+
 const dummy = blogs => 1
 
 const totalLikes = blogs => {
@@ -8,9 +14,33 @@ const totalLikes = blogs => {
 
 const favouriteBlog = blogs => {
 	const likes = blogs.map(blog => blog.likes)
-	return blogs.length === 0
-		? 0
-		: Math.max(...likes)
+	return arrayMax(likes)
 }
 
-module.exports = { dummy, totalLikes, favouriteBlog }
+const mostBlogs = blogs => {
+	const uniqueAuthors = {}
+	blogs.forEach(blog => {
+		if (blog.author in uniqueAuthors) {
+			uniqueAuthors[blog.author] += 1
+		} else {
+			uniqueAuthors[blog.author] = 1
+		}
+	})
+	const uniqueArray = Object.values(uniqueAuthors)
+	return arrayMax(uniqueArray)
+}
+
+const mostLikes = blogs => {
+	const uniqueAuthors = {}
+	blogs.forEach(blog => {
+		if (blog.author in uniqueAuthors) {
+			uniqueAuthors[blog.author] += blog.likes
+		} else {
+			uniqueAuthors[blog.author] = blog.likes
+		}
+	})
+	const uniqueArray = Object.values(uniqueAuthors)
+	return arrayMax(uniqueArray)
+}
+
+module.exports = { dummy, totalLikes, favouriteBlog, mostBlogs, mostLikes }
