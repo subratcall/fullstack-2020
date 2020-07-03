aea0960c903aeea10887f03fd1923e571268d212
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-07-03 16:45:02 -0400

    6.21

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,25 +1,20 @@
 const initialNotif = 'No new notifications'
+let id
 
 export const setNotif = (notif, seconds) => {
 	return async dispatch => {
 		dispatch({ type: 'SET_NOTIF', notif })
-		dispatch({ type: 'CLEAR' })
-		const id = setTimeout(
+		if (id) clearTimeout(id)
+		id = setTimeout(
 			() => dispatch({ type: 'SET_NOTIF', notif: initialNotif }),
 			seconds * 1000)
-		dispatch({ type: 'SET_ID', id })
 	}
 }
 
 const reducer = (state = initialNotif, action) => {
 	switch (action.type) {
 		case "SET_NOTIF":
-			return { ...state, message: action.notif }
-		case "SET_ID":
-			return { ...state, id: action.id }
-		case "CLEAR":
-			if (state.id) clearTimeout(state.id)
-			return state
+			return action.notif
 		default:
 			return state
 	}
