435ae4425d127411cdff454f3fa1128dfb4ddd38
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2020-08-03 13:09:27 -0400

    7.13

--- part7/bloglist-redux/frontend/src/reducers/userReducer.js
+++ part7/bloglist-redux/frontend/src/reducers/userReducer.js
@@ -1,8 +1,6 @@
 import userService from '../services/users'
 
 const INIT = 'INIT_USERS'
-const ADD = 'ADD_USER'
-const DELETE = 'DELETE_USER'
 
 export const initUsers = () => {
 	return async dispatch => {
@@ -11,29 +9,10 @@ export const initUsers = () => {
 	}
 }
 
-export const createUser = (blog) => {
-	return async dispatch => {
-		const newBlog = await userService.create(blog)
-		dispatch({ type: ADD, data: newBlog })
-	}
-}
-
-export const deleteUser = (id) => {
-	return async dispatch => {
-		const deleted = await userService.remove(id)
-		dispatch({ type: DELETE, data: deleted })
-	}
-}
-
 const reducer = (state = [], action) => {
 	switch (action.type) {
 		case INIT:
 			return action.data
-		case ADD:
-			return state.concat(action.data)
-		case DELETE:
-			const deleted = action.data
-			return state.filter(b => b.id !== deleted.id)
 		default:
 			return state
 	}
